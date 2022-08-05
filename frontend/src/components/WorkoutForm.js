8642918d6cfdcce74dab48616675c05df2c59d50
Ty <tbaqry@gmail.com>
2022-08-04 22:01:04 -0700

    workoutcontext and useworkout context has been added with modified in the other files

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,20 +1,25 @@
-import { useState } from "react"
+import { useState } from 'react'
+import {useWorkoutsContext} from '../hooks/useWorkoutContext'
+
 
 const WorkoutForm = () => {
+	const{ dispatch } = useWorkoutsContext()
+
 	const [title, setTitle] = useState('')
 	const [load, setLoad] = useState('')
 	const [reps, setReps] = useState('')
 	const [error, setError] = useState(null)
+	const [emptyFields, setEmptyFields] = useState([])
 
-	const handleSubmit= async(e) => {
+	const handleSubmit = async(e) =>{
 		e.preventDefault()
 
-		const workout = {title, load, reps}
+		const workout ={title, load, reps}
 
 		const response = await fetch('/api/workouts', {
 			method: 'POST',
-			body:json.stringify(workout),
-			headers:{
+			body: JSON.stringify(workout),
+			headers: {
 				'Content-Type': 'application/json'
 			}
 		})
@@ -22,43 +27,51 @@ const WorkoutForm = () => {
 
 		if(!response.ok){
 			setError(json.error)
+			setEmptyFields(json.emptyFields)
 		}
 		if(response.ok){
 			setTitle('')
 			setLoad('')
 			setReps('')
 			setError(null)
+			setEmptyFields([])
+			console.log('new workout added', json)
+			dispatch({type: 'CREATE_WORKOUT',payload: json})
 		}
 	}
 
 	return(
-			<form className="create" onSubmit={handleSubmit}>
-				<h3>Add a New Workout<h3>
-				<label>Exercise Title: </label>
-				<input 
-					type="text"
-					onChange={(e) => setTitle(e.target.value)}
-					value={title}
-				/>
+		<form className="create" onSubmit={handleSubmit}>
+			<h3>Add a New Workout</h3>
 
-				<label>Load(in kg): </label>
-				<input 
-					type="number"
-					onChange={(e) => setLoad(e.target.value)}
-					value={load}
-				/>
+			<label>Exercise Title: </label>
+			<input 
+				type="text"
+				onChange={(e) => setTitle(e.target.value)}
+				value={title}
+				className={emptyFields.includes('title') ? 'error' : ''}
+			/>
 
-				<label>Reps: </label>
-				<input 
-					type="number"
-					onChange={(e) => setReps(e.target.value)}
-					value={reps}
-				/>
+			<label>Load (in Kg): </label>
+			<input
+				type="number"
+				onChange={(e) => setLoad(e.target.value)}
+				value={load}
+				className={emptyFields.includes('load') ? 'error' : ''}
+			/>
 
-				<button>Add Workout</button>
-				{error && <div className="error">{error}</div>}
-			</form>
+			<label>Reps: </label>
+			<input 
+				type="number"
+				onChange={(e) => setReps(e.target.value)}
+				value={reps}	
+				className={emptyFields.includes('reps') ? 'error' : ''}
+			/>
+
+			<button>Add Workout</button>
+			{error && <div className="error">{error}</div>}
+		</form>
 		)
 }
 
-export default WorkoutForm;
+export default WorkoutForm
